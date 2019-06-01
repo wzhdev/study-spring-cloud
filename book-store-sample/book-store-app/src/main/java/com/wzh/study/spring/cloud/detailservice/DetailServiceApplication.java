4b506000c716798a12ef50f62c2f0ac99ece042b
wzhdev <wzh_dev@163.com>
2019-06-01 23:22:45 +0800

    init project

---
+++ book-store-sample/book-store-app/src/main/java/com/wzh/study/spring/cloud/detailservice/DetailServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wzh.study.spring.cloud.detailservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetailServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DetailServiceApplication.class, args);
+	}
+
+}
