5f1303b70a63cf8de9d597cf5c0729ef2da33032
wzhdev <wzh_dev@163.com>
2019-06-02 03:14:22 +0800

    consul

--- book-store-sample/book-detail-service/src/main/java/com/wzh/study/spring/cloud/controller/BookDetailController.java
+++ book-store-sample/book-detail-service/src/main/java/com/wzh/study/spring/cloud/controller/BookDetailController.java
@@ -1,4 +1,35 @@
 package com.wzh.study.spring.cloud.controller;
 
+import com.wzh.study.spring.cloud.domain.BookDetail;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+@RequestMapping("/api/v1")
 public class BookDetailController {
+
+    private Logger logger = LoggerFactory.getLogger(BookDetailController.class);
+
+    @GetMapping("/bookDetail/{id}")
+    @ApiOperation("get Book Detail by id")
+    public ResponseEntity<BookDetail> getBookDetailById(@ApiParam(value = "the book id", type = "int", example = "1") @PathVariable("id") int id) {
+        logger.info("id: {}", id);
+        BookDetail bd = new BookDetail();
+        bd.setId(id);
+        bd.setAuthor("Wang xiaowu");
+        bd.setTitle("Hello World");
+        bd.setPubdate(new Date());
+        logger.info("BookDetail: {}", bd);
+        return new ResponseEntity<>(bd, HttpStatus.OK);
+    }
 }
--- book-store-sample/book-detail-service/src/main/java/com/wzh/study/spring/cloud/domain/BookDetail.java
+++ book-store-sample/book-detail-service/src/main/java/com/wzh/study/spring/cloud/domain/BookDetail.java
@@ -1,6 +1,18 @@
 package com.wzh.study.spring.cloud.domain;
 
+import lombok.Data;
+
+import java.util.Date;
+
 @Data
 public class BookDetail {
+
+    private int id;
+
+    private String title;
+
+    private String author;
+
+    private Date pubdate;
 
 }
